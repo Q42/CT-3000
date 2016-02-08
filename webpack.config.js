636a664cc4e827ef90182a9de49f3bc1289c32fc
Joel de Nes <joeldenes@gmail.com>
2016-02-08 10:04:04 +0100

    Set basic structure
    - node packages
    - webpack config

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var path = require("path");
+
+module.exports = {
+  context: __dirname + "/src",
+  entry: {
+    javascript: "./main.js",
+    html: "./index.html",
+  },
+
+  output: {
+    path: path.resolve(__dirname + "/dist"),
+    filename: "bundle.js"
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel",
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.html$/,
+        loader: "file?name=[name].[ext]",
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style!css!sass'
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|woff)$/,
+        loader: "file?name=img/img-[hash:6].[ext]"
+      }
+    ],
+  }
+};
