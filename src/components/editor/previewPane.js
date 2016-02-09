818a20c4c34cef271171d5280504446c9d19dc13
Guido Bouman <guido@q42.nl>
2016-02-09 18:03:58 +0100

    Remove useless result component.

--- src/components/editor/previewPane.js
+++ src/components/editor/previewPane.js
@@ -1,12 +1,20 @@
 import React from 'react';
-
-import Result from './result';
+import * as Objects from './objects/_index';
 
 export default React.createClass({
+  getInitialState() {
+    const objects = Object.keys(Objects).map((key) => {
+      const ObjectInstance = Objects[key];
+      return <ObjectInstance key={key} />
+    });
+
+    return { objects }
+  },
+
   render() {
     return(
       <div className="pane preview-pane">
-        <Result />
+        {this.state.objects}
       </div>
     );
   }
