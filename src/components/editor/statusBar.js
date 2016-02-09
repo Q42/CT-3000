28727d05c7c093137bcc002bac7b2e515b21586d
huijkman <johan@q42.nl>
2016-02-09 12:23:18 +0100

    WIP: spring in statusbar

--- src/components/editor/statusBar.js
+++ src/components/editor/statusBar.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import BaseElement from './baseElement.js';
+import { spring, presets } from 'react-motion';
 
-export default React.createClass({
+export default React.createClass ({
+  getInitialState() {
+    return {
+      items: Array.from(Array(8).keys())
+    }
+  },
+
+  getDefaults() {
+    let obj = {};
+    this.state.items.forEach(key =>{
+      obj[key] = {
+        val: {
+          rotate: -360,
+          scale: 0
+        }
+      }
+    });
+    return obj;
+  },
+
+  getEnds() {
+    let obj = {};
+    this.state.items.forEach(key =>{
+      obj[key] = {
+        val: {
+          rotate: 0,
+          scale: 1
+        }
+      }
+    });
+    return obj;
+  },
+
   render() {
     return(
-      <div className="status-bar">Status bar</div>
+        <spring
+          defaultValue={this.getDefaults()}
+          endValues={this.getEnds()}>
+          <div className="status-bar">
+            {this.state.items.map(key => {
+              return <BaseElement key={key}>{key}</BaseElement>
+            })}
+          </div>
+        </spring>
     );
   }
 });
