e98aee4b4d9524d62208a6dd413e9e6df48e5d16
Guido Bouman <guido@q42.nl>
2016-02-10 11:48:31 +0100

    Remove some objects for now.

--- src/components/editor/objects/_index.js
+++ src/components/editor/objects/_index.js
@@ -2,7 +2,5 @@ export {default as Lamp} from './lamp';
 export {default as Deur} from './deur';
 export {default as Klok} from './klok';
 export {default as TV} from './tv';
-export {default as Fiets} from './fiets';
-export {default as Kat} from './kat';
 export {default as Telefoon} from './telefoon';
 export {default as Weer} from './weer';
