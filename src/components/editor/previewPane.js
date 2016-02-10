9f10767eea1ed3fd8f5765d763db4e22251c0e9d
Guido Bouman <guido@q42.nl>
2016-02-10 13:56:36 +0100

    Render items in previewPane based on state.

--- src/components/editor/previewPane.js
+++ src/components/editor/previewPane.js
@@ -1,21 +1,62 @@
 import React from 'react';
 import * as Objects from './objects/_index';
 
-export default React.createClass({
-  getInitialState() {
-    const objects = Object.keys(Objects).map((key) => {
-      const ObjectInstance = Objects[key];
-      return <ObjectInstance key={key} />
-    });
-
-    return { objects }
-  },
+export default class extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      rowResult: {
+        checks: [
+          {
+            object: 'Lamp',
+            value: 'aan',
+            valid: true
+          },
+          {
+            object: 'Lamp',
+            value: 'uit',
+            valid: false
+          }
+        ],
+        assignments: [
+          {
+            object: 'Deur',
+            value: 'aan',
+            valid: false
+          },
+          {
+            object: 'Deur',
+            value: 'open',
+            valid: true
+          }
+        ]
+      }
+    }
+  }
 
   render() {
+    const checks = this.state.rowResult.checks.map((check, i) => {
+      const ObjectRef = Objects[check.object];
+      return <ObjectRef key={i} />
+    });
+
+    const assignments = this.state.rowResult.assignments.map((assignment, i) => {
+      const ObjectRef = Objects[assignment.object];
+      return <ObjectRef key={i} />
+    });
+
     return(
       <div className="pane preview-pane">
-        {this.state.objects}
+        <div className="objects checks">
+          {checks}
+        </div>
+        <div className="computer">
+          // This is our PC component //
+        </div>
+        <div className="objects assignments">
+          {assignments}
+        </div>
       </div>
     );
   }
-});
+};
