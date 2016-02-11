8e451070359f07424b475aa61b08999db52ee9ac
huijkman <johan@q42.nl>
2016-02-11 20:42:03 +0100

    animated speech bubbles

--- src/components/viewer.js
+++ src/components/viewer.js
@@ -3,6 +3,7 @@ import Rebase from 're-base';
 import { Motion, spring, presets } from 'react-motion';
 
 const base = Rebase.createClass('https://blink-ct.firebaseio.com/classes/1');
+const springSetting1 = {stiffness: 164, damping: 10};
 
 export default class Viewer extends React.Component {
   constructor(props) {
@@ -26,14 +27,14 @@ export default class Viewer extends React.Component {
 
   render() {
     let o = this.state.display.message == undefined ||  this.state.display.message == '' ? 0 : 1;
-    let x = this.state.display.message == undefined ||  this.state.display.message == '' ? 100 : 0;
+    let t = this.state.display.message == undefined ||  this.state.display.message == '' ? -100 : 0;
 
     return(
       <div className="viewer">
         <div className="content">
 
-        <Motion defaultStyle={{opacity: 0 }} style={{opacity: spring(o)}}>
-            {interpolatingStyle => <div className="message" style={interpolatingStyle}>
+        <Motion defaultStyle={{ x: 0, y: -100, z: 0 }} style={{ x: spring(o), y: spring(t,springSetting1), z: spring(o,springSetting1)}}>
+            {value => <div className="message" style={{opacity: value.x, transform: 'scale(' + value.z + ') translateX(' + value.y + 'px)'}}>
               {this.state.display.message}
             </div>}
           </Motion>
