01002db3ad9f14856bb2fb3b57c42c55a08976bf
huijkman <johan@q42.nl>
2016-02-11 17:17:26 +0100

    Message workses

--- src/components/editor/objects/bericht.js
+++ src/components/editor/objects/bericht.js
@@ -3,9 +3,14 @@ import { BaseObject } from './_baseObject';
 
 class Bericht extends React.Component {
   render() {
+    let state;
+    if(this.props.data.object)
+      state = this.props.data.object.state;
+
     return <div className="icon">
       <div className="on"></div>
       <div className="off"></div>
+      <div className="message"><p>{state}</p></div>
     </div>;
   }
 }
