514dcf7d0c3b2ac6a6fced29f24a79c3563c281a
huijkman <johan@q42.nl>
2016-02-12 16:56:39 +0100

    WIP: message

--- src/components/editor/objects/bericht.js
+++ src/components/editor/objects/bericht.js
@@ -2,9 +2,17 @@ import React from 'react';
 import { BaseObject } from './_baseObject';
 import Rebase from 're-base';
 
+import { Motion, spring, presets } from 'react-motion';
+
+const springSetting1 = {stiffness: 164, damping: 10};
+
 class Bericht extends React.Component {
   constructor(props) {
     super(props);
+
+    this.o = 0;
+    this.t = -100;
+    this.s = 100;
 
     this.state = {
       display: {},
@@ -42,10 +50,18 @@ class Bericht extends React.Component {
     if(this.props.data.object)
       state = this.props.data.object.state;
 
+    this.o = state == undefined ||  state == '' && this.state.show ? 0 : 1;
+    this.t = state == undefined ||  state == '' && this.state.show ? -100 : 40;
+    this.s = state == undefined ||  state == '' && this.state.show ? 100 : -70;
+
     return <div className="icon">
       <div className="on"></div>
       <div className="off"></div>
-      <div className="message"><p>{state}</p></div>
+      <Motion defaultStyle={{ x: 0, y: -100, z: 0, q:100 }} style={{ x: spring(this.o), y: spring(this.t,springSetting1), z: spring(this.o,springSetting1),q: spring(this.s,springSetting1)}}>
+        {value => <div className="message" style={{opacity: value.x, transform: 'scale(' + value.z + ') translate(' + value.y + 'px,'+ value.q +'px)'}}>
+          <p>{state}</p>
+        </div>}
+      </Motion>
     </div>;
   }
 }
