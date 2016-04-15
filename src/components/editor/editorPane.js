02c343e0ab79127b580ed52080f7e2030b93afaa
huijkman <johan@q42.nl>
2016-04-15 12:52:26 +0200

    uitgecommentarieerd: voorbeeld code

--- src/components/editor/editorPane.js
+++ src/components/editor/editorPane.js
@@ -40,8 +40,9 @@ export default class EditorPane extends React.Component {
 
     this.cm = this.refs.editor.getCodeMirror();
     this.cm.on('cursorActivity', this.parseLine);
-    const doc = this.cm.getDoc();
-    doc.addLineClass(0,"wrap","syntax-error");
+    // Example 'syntax error':
+    //const doc = this.cm.getDoc();
+    //doc.addLineClass(0,"wrap","syntax-error");
   }
 
   componentWillUnmount() {
