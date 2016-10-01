8a97c428eecbb62e69c8d0144a3c55e177d683d0
Lukas van Driel <lukas@q42.nl>
2016-10-01 10:59:18 +0200

    placed a release in gh-pages

--- dist/src/components/editor/objects/_index.js
+++
@@ -1,7 +0,0 @@
-export {default as Lamp} from './lamp';
-export {default as Deur} from './deur';
-export {default as Bericht} from './bericht';
-export {default as Tijd} from './tijd';
-export {default as Muziek} from './muziek';
-export {default as Kat} from './kat';
-export {default as Weer} from './weer';
