e3452f4cb5f80c0d0a84c149fb0f7d3b2e83f316
Lukas van Driel <lukas@q42.nl>
2018-05-16 21:34:53 +0200

    img optimization removed use of specific background for homepage

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -35,12 +35,12 @@ export default class LandingPage extends React.Component {
             <article>
               <h2>Programmeer je Smart Life</h2>
               <p>Wil je iets bedenken voor een <span className="color-red">Smart Life</span> of een <span className="color-red">Smart Home</span>? Wil jij bijvoorbeeld dat de <span className="color-blue">muziek</span> aan gaat <span className="color-red">als</span> de zon schijnt? Of moet de <span className="color-blue">radio</span> je wekken als het <span className="color-purple">7.00</span> in de ochtend is, en weer zelf uit gaan als je om <span className="color-purple">8.15</span> naar school gaat? Of wil je een <span className="color-blue">sms</span> ontvangen <span className="color-red">als</span> iemand je <span className="color-blue">voordeur</span> open doet?</p>
-              <p>Dan is het handig <span className="color-red">als</span> je kunt <span className="color-blue">programmeren</span>! Met de programmeer-tool <span className="color-purple">CT3000</span> oefen je een aantal belangrijke programmeer-principes, zoals <span className="color-blue">‘IF-THEN’</span>. Dus, bedenk iets slims <span className="color-red">en</span> programmeer het zelf!</p>
-              <Link to={'/nl/tool'} className="fat-link"><span>Aan de slag met <em>CT3000</em></span></Link>
+              <p>Dan is het handig <span className="color-red">als</span> je kunt <span className="color-blue">programmeren</span>! Met de programmeer-tool <span className="color-purple">CT-3000</span> oefen je een aantal belangrijke programmeer-principes, zoals <span className="color-blue">‘IF-THEN’</span>. Dus, bedenk iets slims <span className="color-red">en</span> programmeer het zelf!</p>
+              <Link to={'/nl/tool'} className="fat-link"><span>Aan de slag met <em>CT-3000</em></span></Link>
             </article>
             <article>
               <h2>Het thema Smart Life</h2>
-              <p>De programmeer-tool CT3000 hoort bij het thema Smart Life, over computional thinking en programmeren.
+              <p>De programmeer-tool CT-3000 hoort bij het thema Smart Life, over computional thinking en programmeren.
                 Het thema is ontwikkeld door uitgeverij <a href="http://blink.nl" className="inline-link">Blink</a>
                 , <a href="http://codeuur.nl" className="inline-link">stichting CodeUur</a> en <a href="http://q42.nl" className="inline-link">Q42</a>.</p>
               <p>Het thema Smart Life omvat vijf lessen en is ontwikkeld voor groep 5/6 en
@@ -48,7 +48,7 @@ export default class LandingPage extends React.Component {
                 zijn gratis beschikbaar voor alle scholen.</p>
               <p><i>De lessen zijn gratis beschikbaar via onderstaande de knop. Het thema is
                 ook toegankelijk in de lesomgeving van Blink: <a href="http://www.blink-studio.nl/" className="inline-link">Blink Studio</a>.</i></p>
-              <a href="https://smart-life.blink.nl" className="fat-link"><span>Naar de <em>lessen</em></span></a>
+              <a href="https://smart-life.blink.nl/bwonderbouw/7-8/smart-life" className="fat-link"><span>Naar de <em>lessen</em></span></a>
             </article>
           </div>
         </div>
